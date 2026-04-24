17f6806324beac4ae97ac14349cb10dbba3f8a0d
Muhammad Ilham Setiawan <m.ilsetiawan1@gmail.com>
2026-04-24 15:32:47 +0700

    chore: setup tailwind css and vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        laravel(["resources/css/app.css", "resources/js/app.js"]),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
         tailwindcss(),
     ],
 });
